27a626bb7ecce82cf29389a90a49bcea44d131d0
Fotis Gimian <fgimiansoftware@gmail.com>
2014-02-21 12:00:01 +1100

    Updated code to pass flake8 tests

--- test/test_app.py
+++ test/test_app.py
@@ -4,38 +4,38 @@ import pytest
 
 
 def create_good_app():
-  app = Flask(__name__)
+    app = Flask(__name__)
 
-  @app.route('/')
-  def index():
-    return 'Hello'
+    @app.route('/')
+    def index():
+        return 'Hello'
 
-  return app
+    return app
 
 
 def test_good_app():
-  app = failsafe(create_good_app)()
-  rv = app.test_client().get('/')
-  assert b'Hello' in rv.data
+    app = failsafe(create_good_app)()
+    rv = app.test_client().get('/')
+    assert b'Hello' in rv.data
 
 
 class MyException(Exception):
-  pass
+    pass
 
 
 def create_bad_app():
-  raise MyException()
+    raise MyException()
 
 
 def test_bad_app_without_failsafe():
-  with pytest.raises(MyException):
-    create_bad_app()
+    with pytest.raises(MyException):
+        create_bad_app()
 
 
 def test_bad_app_with_failsafe():
-  # creating the app should not raise an exception
-  app = failsafe(create_bad_app)()
+    # creating the app should not raise an exception
+    app = failsafe(create_bad_app)()
 
-  # but fetching the page should
-  with pytest.raises(MyException):
-    app.test_client().get('/')
+    # but fetching the page should
+    with pytest.raises(MyException):
+        app.test_client().get('/')
